2a04e9db6a7b204bae2e26cc82066c606a329a49
Ragil14 <rahil.gilani@gmail.com>
2022-04-25 14:17:04 -0500

    edit icon file paths

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -7,15 +7,15 @@ const FILES_TO_CACHE = [
   '/manifest.json',
   '/js/idb.js',
   '/js/index.js',
-  '/css/style.css',
+  '/css/styles.css',
   '/icons/icon-72x72.png',
   '/icons/icon-96x96.png',
-  '/images/icons/icon-128x128.png',
-  '/images/icons/icon-144x144.png',
-  '/images/icons/icon-152x152.png',
-  '/images/icons/icon-192x192.png',
-  '/images/icons/icon-384x384.png',
-  '/images/icons/icon-512x512.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png'
 ];
 
 // Install the service worker
